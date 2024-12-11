8b45f2878a2fdcf5fa00958668e51837644590c9
Oddvar Lia (ST MSU GEO) <olia@equinor.com>
2024-12-11 12:10:01 +0100

    renaming

--- src/fmu/tools/rms/__init__.py
+++ src/fmu/tools/rms/__init__.py
@@ -6,8 +6,8 @@ from .generate_petro_jobs_for_field_update import (
     main as generate_petro_jobs,
 )
 from .import_localmodules import import_localmodule
-from .update_petro_real_from_fields_per_facies import (
-    main as update_petro_real,
+from .update_petro_real import (
+    main as update_petro_realizations,
 )
 from .volumetrics import rmsvolumetrics_txt2df
 
@@ -15,5 +15,5 @@ __all__ = [
     "rmsvolumetrics_txt2df",
     "import_localmodule",
     "generate_petro_jobs",
-    "update_petro_real",
+    "update_petro_realizations",
 ]
